62059ff5510492b30439f90c5f421b30e3c8506f
KreiosX <103337925+KreiosX@users.noreply.github.com>
2022-05-07 17:58:41 +0200

    Update EditSeats.xaml.cs
    CS

--- EditSeats.xaml.cs
+++ EditSeats.xaml.cs
@@ -22,7 +22,7 @@ namespace AsilCinemaTicketAutomation
     /// </summary>
     public partial class EditSeats : UserControl
     {
-        SqlConnection ActiveConnection = new SqlConnection("Server=79.123.228.28,5501;Database=Gunduz_HAcku;User Id=hacku;Password=x7a45tt55;");
+        SqlConnection ActiveConnection = new SqlConnection("--");
         public EditSeats()
         {
             InitializeComponent();
